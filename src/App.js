c7ae8921167d09e135e907bcd323e5f076f4c41e
aman-alem <alem.aman@gmail.com>
2022-09-12 14:42:54 -0500

    Posts state works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { Route, Routes, Navigate, UseNavigate } from "react-router-dom";
 import axios from "axios";
+import Nav from "./components/Nav";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   const [posts, setPosts] = useState({});
 
   useEffect(() => {
-    console.log("posts useEffect ran!");
-    fetch("http://localhost:8000/blogist/posts-list/")
-      .then((res) => res.json())
-      .then((res) => {
-        setPosts(res);
-        return res;
-      })
-      .then((res) => console.log(res));
+    axios.get("http://localhost:8000/blogist/posts-list/").then(({ data }) => {
+      setPosts(data).then(() => console.log("posts useEffect ran!"));
+    });
   }, []);
 
   return (
     <div className="App">
-      <h1>Post List</h1>
+      <h1>App.js</h1>
+      <Nav />
+      <Routes>
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavDiv = styled.div`
+  ul {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    list-style-type: none;
+  }
+`;
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <NavDiv>
+      <ul>
+        <li>
+          <Link to="/Admin">Admin</Link>
+        </li>
+      </ul>
+    </NavDiv>
+  );
 };
 
 export default Nav;
