8007d9e4b6e8a304459d53138fc29bd9f4bb0522
aman-alem <alem.aman@gmail.com>
2022-09-12 14:06:01 -0500

    pages added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState({});
+
+  useEffect(() => {
+    console.log("posts useEffect ran!");
+    fetch("http://localhost:8000/blogist/posts-list/")
+      .then((res) => res.json())
+      .then((res) => {
+        setPosts(res);
+        return res;
+      })
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Post List</h1>
     </div>
   );
 }
